08ef6021dcde4dbb3c95ab6570c790ae1187e6b4
Raimi Azeez Babatunde <61598894+raimiazeez26@users.noreply.github.com>
2022-11-14 11:35:02 +0100

    Add files via upload

--- approval_app.py
+++ approval_app.py
@@ -1,3 +1,4 @@
+#try with live update breakdown - Final DONE
 
 import pandas as pd
 import base64
@@ -15,20 +16,10 @@ columns = ["symbol", "Lots", "Position", "Confluence", "Candlestick", "Zone In/O
                     "P/A Pattern", "Analyst", "Approval", "SL", "TP","Comment" ]
 buy_sell_list = ["Buy", "Sell"]
 
-app = Dash(__name__, external_stylesheets=[dbc.themes.CYBORG]) #dbc.themes.ZEPHYR]
+app = Dash(__name__, external_stylesheets=[dbc.themes.CYBORG])
 server = app.server
 
-def layout_function():
-    #url = 'https://raw.githubusercontent.com/raimiazeez26/approval_app/main/approval.csv'
-    url = 'approval.csv'
-    try:
-        data = pd.read_csv(url).to_dict('records')
-    except:
-        data=[
-                {}
-            ]
-    
-    return html.Div([
+app.layout = html.Div([
         html.Div([
             html.Img(src='data:image/png;base64,{}'.format(test_base64),
                     style={'height':'25%', 'width':'25%'}),
@@ -54,109 +45,20 @@ def layout_function():
         html.Hr(),
 
 
-        html.Div([
+        html.Div(id='adding-rows',
 
-            dash_table.DataTable(
-            id='adding-rows-table',
-            css=[{"selector":".dropdown", "rule": "position: static"}],
-            #css=( {"selector": ".Select-menu-outer", "rule": 'display : block !important'}),
-            
-            columns = [{'id': 'Symbol', 'name': 'Symbol'},
-            {'id': 'Lots', 'name': 'Lots'},
-            {'id': 'Position', 'name': 'Position', 'presentation': 'dropdown'},
-            {'id': 'Confluence', 'name': 'Confluence', 'presentation': 'dropdown'},
-            {'id': 'Candlestick', 'name': 'Candlestick', 'presentation': 'dropdown'},
-            {'id': 'Zone In/Out', 'name': 'Zone In/Out', 'presentation': 'dropdown'},
-            {'id': 'P/A Pattern', 'name': 'P/A Pattern', 'presentation': 'dropdown'},
-            {'id': 'Analyst', 'name': 'Analyst', 'presentation': 'dropdown'},
-            {'id': 'Approval', 'name': 'Approval', 'presentation': 'dropdown'},
-            {'id': 'SL', 'name': 'SL'},
-            {'id': 'TP', 'name': 'TP'},
-            {'id': 'Comment', 'name': 'Comment'},
-                       ],
-
-            data=data,
-            editable=True,
-            row_deletable=True,
-                
-            dropdown={
-            'Position': {
-                'options': [
-                    {'label': i, 'value': i}
-                    for i in buy_sell_list
-                ]
-            },
-            'Confluence': {
-                 'options': [
-                    {'label': i, 'value': i}
-                    for i in ["Both", "One", "None"]
-                ]
-            },
-            'Candlestick': {
-                 'options': [
-                    {'label': i, 'value': i}
-                    for i in ["Both TF", "One TF", "None"]
-                ]
-            },
-            'Zone In/Out': {
-                 'options': [
-                    {'label': i, 'value': i}
-                    for i in ["In", "Out"]
-                ]
-            },
-            'P/A Pattern': {
-                 'options': [
-                    {'label': i, 'value': i}
-                    for i in ["Yes", "No"]
-                ]
-            },
-            'Analyst': {
-                 'options': [
-                    {'label': i, 'value': i}
-                    for i in ["Trader F.A.", "Trader I.J.", "Trader G.O.",
-                             "Trader B.E.","Trader M.O.","Trader R.A."]
-                ]
-            },
-            'Approval': {
-                 'options': [
-                    {'label': i, 'value': i}
-                    for i in ["Approved", "Not Approved" ]
-                ]
-            },
-        },
-
-            style_cell={'textAlign': 'center',
-                       #'backgroundColor': '#36454F',
-                        'color': 'white'},
-
-            style_header={
-                    'backgroundColor': 'rgb(30, 30, 30)',
-                    'color': 'white','fontWeight': 'bold'}, 
-
-            style_data={
-                    'backgroundColor': '#000000',
-                    'color': 'white'
-                        },
-        ),
-
-        html.Button('Add Row', id='editing-rows-button', n_clicks=0),
-        html.Button('Request/Send Approval', id='approval-button', n_clicks=0),
-        html.Div(id="send_request",children="Press button to send Request"),
-        dcc.Interval(id='update', interval=1000, n_intervals=0)
-
-        ],style={'margin-left': '10%', 
-                       'margin-right': '10%', 
-                       'margin-top': '20px',
-                       'text_align' : 'center',
-                       #'color' : '#ff0000',
-                       #'border': '1px solid red',
-                        "height": "90vh", "maxHeight": "900vh"
-                      }),
+                style={'margin-left': '10%', 
+                               'margin-right': '10%', 
+                               'margin-top': '20px',
+                               'text_align' : 'center',
+                               #'color' : '#ff0000',
+                               #'border': '1px solid red',
+                                "height": "90vh", "maxHeight": "900vh"
+                              }),
         
+        dcc.Interval(id='update', interval=25000, n_intervals=0)
         
         ])
-
-app.layout = layout_function
 
 @app.callback(
     Output('adding-rows-table', 'data'),
@@ -176,13 +78,120 @@ def add_row(n_clicks, rows, columns):
     State('adding-rows-table', 'data'))
 
 def update_table(nclicks,table1):
-    #url = 'https://raw.githubusercontent.com/raimiazeez26/approval_app/main/approval.csv'
     url = 'approval.csv'
     if nclicks == 0:
         raise PreventUpdate
     else:
         pd.DataFrame(table1).to_csv(url, index=False)
         return "Request/Approval Sent"
+
+@app.callback(
+    Output('adding-rows', 'children'),
+    Input('update', 'n_intervals'),
+    #State('adding-rows-table', 'data')
+)
+
+def update(n):
+
+    return [
+        dbc.Container([
+                dash_table.DataTable(
+                id='adding-rows-table',
+                #css=[{"selector":".dropdown", "rule": "position: static"}],
+                css=[ { "selector": ".dash-spreadsheet-container .Select-value-label", "rule": "color: white" }, ],
+                #css=( {"selector": ".Select-menu-outer", "rule": 'display : block !important'}),
+
+                columns = [{'id': 'Symbol', 'name': 'Symbol'},
+                {'id': 'Lots', 'name': 'Lots'},
+                {'id': 'Position', 'name': 'Position', 'presentation': 'dropdown'},
+                {'id': 'Confluence', 'name': 'Confluence', 'presentation': 'dropdown'},
+                {'id': 'Candlestick', 'name': 'Candlestick', 'presentation': 'dropdown'},
+                {'id': 'Zone In/Out', 'name': 'Zone In/Out', 'presentation': 'dropdown'},
+                {'id': 'P/A Pattern', 'name': 'P/A Pattern', 'presentation': 'dropdown'},
+                {'id': 'Analyst', 'name': 'Analyst', 'presentation': 'dropdown'},
+                {'id': 'Approval', 'name': 'Approval', 'presentation': 'dropdown'},
+                {'id': 'SL', 'name': 'SL'},
+                {'id': 'TP', 'name': 'TP'},
+                {'id': 'Comment', 'name': 'Comment'},
+                           ],
+
+                data = pd.read_csv('approval.csv').to_dict('records'),
+                editable=True,
+                row_deletable=True,
+
+                dropdown={
+                'Position': {
+                    'options': [
+                        {'label': i, 'value': i}
+                        for i in buy_sell_list
+                    ]
+                },
+                'Confluence': {
+                     'options': [
+                        {'label': i, 'value': i}
+                        for i in ["Both", "One", "None"]
+                    ]
+                },
+                'Candlestick': {
+                     'options': [
+                        {'label': i, 'value': i}
+                        for i in ["Both TF", "One TF", "None"]
+                    ]
+                },
+                'Zone In/Out': {
+                     'options': [
+                        {'label': i, 'value': i}
+                        for i in ["In", "Out"]
+                    ]
+                },
+                'P/A Pattern': {
+                     'options': [
+                        {'label': i, 'value': i}
+                        for i in ["Yes", "No"]
+                    ]
+                },
+                'Analyst': {
+                     'options': [
+                        {'label': i, 'value': i}
+                        for i in ["Trader F.A.", "Trader I.J.", "Trader G.O.",
+                                 "Trader B.E.","Trader M.O.","Trader R.A."]
+                    ]
+                },
+                'Approval': {
+                     'options': [
+                        {'label': i, 'value': i}
+                        for i in ["Approved", "Not Approved" ]
+                    ]
+                },
+            },
+
+                style_cell={'textAlign': 'center',
+                           #'backgroundColor': '#36454F',
+                            'color': 'white'},
+
+                style_header={
+                        'backgroundColor': 'rgb(30, 30, 30)',
+                        'color': 'white','fontWeight': 'bold'}, 
+
+                style_data={
+                        'backgroundColor': '#000000',
+                        'color': 'white'
+                            },
+            ),
+
+            html.Button('Add Row', id='editing-rows-button', n_clicks=0),
+            html.Button('Request/Send Approval', id='approval-button', n_clicks=0),
+            html.Div(id="send_request",children="Press button to send Request"),
+
+            ],style={'margin-left': '10%', 
+                           'margin-right': '10%', 
+                           'margin-top': '20px',
+                           'text_align' : 'center',
+                           #'color' : '#ff0000',
+                           #'border': '1px solid red',
+                            "height": "90vh", "maxHeight": "900vh"
+                          }),
+        ]
     
     
 if __name__ == '__main__':
